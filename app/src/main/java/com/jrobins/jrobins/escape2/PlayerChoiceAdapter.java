8398df26e1cec52fc1b9f9ed4ea3e8989a4059e8
Jessica Robins <jessrrobins@gmail.com>
2014-11-29 14:59:07 -0500

    setting the button to the first letter works

--- app/src/main/java/com/jrobins/jrobins/escape2/PlayerChoiceAdapter.java
+++ app/src/main/java/com/jrobins/jrobins/escape2/PlayerChoiceAdapter.java
@@ -1,10 +1,15 @@
 package com.jrobins.jrobins.escape2;
 
 import android.app.Activity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import com.jrobins.jrobins.escape2.R;
 
@@ -18,7 +23,44 @@ public class PlayerChoiceAdapter extends ArrayAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent){
-        return inflater.inflate(R.layout.player_choice, parent, false);
+
+        if(convertView == null)
+        {
+            convertView  = (LinearLayout)inflater.inflate(R.layout.player_choice, parent, false);
+        }
+
+        //Button yourButton= (Button)  convertView  .findViewById(R.id.YOUR_BUTTON_ID);
+        //yourButton.setOnClickListener(new OnClickListener()
+
+        /*
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // Your code that you want to execute on this button click
+                Intent myIntent = new Intent(CurrentActivity.this, NextActivity.class);
+                CurrentActivity.this.startActivity(myIntent);
+
+            }
+
+        });*/
+
+        final EditText playerName = (EditText) convertView.findViewById(R.id.playerName);
+        final Button button = (Button) convertView.findViewById(R.id.button);
+
+        playerName.addTextChangedListener(new TextWatcher(){
+            public void afterTextChanged(Editable s) {
+                if(s.length() == 0)
+                    button.setText("X");
+                else
+                    button.setText(playerName.getText().toString().substring(0,1));
+            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+            public void onTextChanged(CharSequence s, int start, int before, int count){}
+        });
+
+        return convertView ;
+        //return inflater.inflate(R.layout.player_choice, parent, false);
     }
 
 }
