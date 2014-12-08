95aaba30c98a353cd4ddfc98c3ab6ca30078a305
Jessica Robins <jessrrobins@gmail.com>
2014-12-08 10:09:26 -0500

    parsing maps works 100 percent now. all the columns this time

--- app/src/main/java/com/jrobins/jrobins/escape2/MapParser.java
+++ app/src/main/java/com/jrobins/jrobins/escape2/MapParser.java
@@ -74,8 +74,9 @@ public class MapParser {
                     //eventType = xpp.nextTag();
                 }
                 else if (tag.equalsIgnoreCase("column")) {
-                    int column = Integer.parseInt(xpp.getAttributeValue(null, "id"));
-                    sectors = parseColumn(sectors, column);
+
+                    sectors = parseColumn(sectors);
+                    break;
                     //eventType = xpp.nextTag();
                 }
                 else {
@@ -88,10 +89,11 @@ public class MapParser {
         return new Map (name, sectors);
     }
 
-    public Sector [][] parseColumn (Sector [][] sectors, int column) throws XmlPullParserException, IOException {
+    public Sector [][] parseColumn (Sector [][] sectors) throws XmlPullParserException, IOException {
 
         String tag;
         Sector sector;
+        int column = 0;
 
         //int eventType = xpp.nextTag();
         int eventType = xpp.getEventType();
@@ -99,6 +101,7 @@ public class MapParser {
             if (eventType == XmlPullParser.START_TAG) {
                 tag = xpp.getName();
                 if (tag.equalsIgnoreCase("column")){
+                    column = Integer.parseInt(xpp.getAttributeValue(null, "id"));
                     eventType = xpp.next();
                     tag = xpp.getName();
                 }
@@ -107,9 +110,13 @@ public class MapParser {
 
 
                     sectors = parseSector(sectors, column);
+
                     //sectors[column][sector.yCoordinate()] = sector;
                     //eventType = xpp.nextTag();
                 }
+
+
+
                 else
                     break;
             }
@@ -140,6 +147,7 @@ public class MapParser {
                 else if (tag.equalsIgnoreCase("type")){
                     type = readText();
                     sectors[column][row] = new Sector(column, row, type);
+                    return sectors;
                 }
                 else
                     break;
@@ -182,160 +190,6 @@ public class MapParser {
     }
 
 
-
-    public List parse(InputStream in) throws XmlPullParserException, IOException {
-        try {
-            XmlPullParser parser = Xml.newPullParser();
-            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
-            parser.setInput(in, null);
-            parser.nextTag();
-            return getMaps(xpp);
-        } finally {
-            in.close();
-        }
-    }
-
-    /*
-    public MapParser(XmlResourceParser parser){
-        this.parser = parser;
-    }*/
-
-/*
-    public List <Map> getMaps() throws XmlPullParserException, IOException {
-
-        return getMaps(xpp);
-    }*/
-
-
-    public List <Map> getMaps(XmlResourceParser parser) throws XmlPullParserException, IOException {
-        List <Map> maps = new ArrayList<Map>();
-        //parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
-
-        parser.next();
-
-        //Log.d("parser", parser.getName());
-        parser.require(XmlPullParser.START_TAG, ns, "Maps");
-        while (parser.next() != XmlPullParser.END_TAG) {
-            if (parser.getEventType() != XmlPullParser.START_TAG) {
-                continue;
-            }
-            String name = parser.getName();
-            // Starts by looking for the entry tag
-            if (name.equals("Map")) {
-                maps.add(readMap(parser));
-            } else {
-                skip(parser);
-            }
-        }
-        return maps;
-    }
-
-    // Parses the contents of a map
-    private Map readMap(XmlPullParser parser) throws XmlPullParserException, IOException {
-        parser.require(XmlPullParser.START_TAG, ns, "Map");
-        String mapName = null;
-        Sector sectors[][] = new Sector[23][14];
-
-
-        while (parser.next() != XmlPullParser.END_TAG) {
-            if (parser.getEventType() != XmlPullParser.START_TAG) {
-                continue;
-            }
-            String name = parser.getName();
-            if (name.equals("name")) {
-                mapName = readName(parser);
-            } else if (name.equals("Column")) {
-                sectors = addColumn(parser, sectors);
-            } else {
-                skip(parser);
-            }
-        }
-        return new Map(mapName, sectors);
-    }
-
-    // Processes the name of the map
-    private String readName(XmlPullParser parser) throws IOException, XmlPullParserException {
-        parser.require(XmlPullParser.START_TAG, ns, "name");
-        String name = readText(parser);
-        parser.require(XmlPullParser.END_TAG, ns, "name");
-        return name;
-    }
-
-    // Processes the columns
-    private Sector[][] addColumn(XmlPullParser parser, Sector[][]sectors) throws IOException, XmlPullParserException {
-        //get the column id
-        Sector sector;
-
-        parser.require(XmlPullParser.START_TAG, ns, "Column");
-
-        int column = Integer.parseInt(parser.getAttributeValue(null, "id"));
-
-        parser.nextTag();
-
-        //find the sector tag
-        sector = readSector(parser, column);
-
-        parser.require(XmlPullParser.END_TAG, ns, "Column");
-
-        //add the sector to the matrix & return it
-        sectors[column][sector.yCoordinate()] = sector;
-        return sectors;
-    }
-
-    // Processes the sector in the column
-    private Sector readSector(XmlPullParser parser, int column) throws IOException, XmlPullParserException {
-        parser.require(XmlPullParser.START_TAG, ns, "Sector");
-        int row = readSectorRow(parser);
-        String sectorType = readSectorType(parser);
-        parser.require(XmlPullParser.END_TAG, ns, "Sector");
-        return new Sector(column, row, sectorType);
-    }
-
-    // sector row - return the int of the row. this is going to be one too high because we
-    //  started at 1. oops. so let's just subtract 1 here and call it a day
-    private int readSectorRow(XmlPullParser parser) throws IOException, XmlPullParserException {
-
-        parser.require(XmlPullParser.START_TAG, ns, "row");
-        String row = readText(parser);
-        parser.require(XmlPullParser.END_TAG, ns, "row");
-        return (Integer.parseInt(row)-1);
-    }
-
-    //sector type
-    private String readSectorType(XmlPullParser parser) throws IOException, XmlPullParserException {
-
-        parser.require(XmlPullParser.START_TAG, ns, "type");
-        String sectorType = readText(parser);
-        parser.require(XmlPullParser.END_TAG, ns, "type");
-        return sectorType;
-    }
-
-    // extracts text values from between tags
-    private String readText(XmlPullParser parser) throws IOException, XmlPullParserException {
-        String result = "";
-        if (parser.next() == XmlPullParser.TEXT) {
-            result = parser.getText();
-            parser.nextTag();
-        }
-        return result;
-    }
-
-    private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
-        if (parser.getEventType() != XmlPullParser.START_TAG) {
-            throw new IllegalStateException();
-        }
-        int depth = 1;
-        while (depth != 0) {
-            switch (parser.next()) {
-                case XmlPullParser.END_TAG:
-                    depth--;
-                    break;
-                case XmlPullParser.START_TAG:
-                    depth++;
-                    break;
-            }
-        }
-    }
 
     private String readText() throws IOException, XmlPullParserException {
         String result = "";
