fd6d2438587d3c0f804874ce091981f9b9ed84a0
Jessica Robins <jessica.robins@sungard.com>
2014-12-16 15:48:36 -0500

    changed it back

--- app/src/main/java/com/jrobins/jrobins/escape2/MapView.java
+++ app/src/main/java/com/jrobins/jrobins/escape2/MapView.java
@@ -604,7 +604,7 @@ public class MapView extends BasicHexagonGridView {
         @Override
         public boolean onSingleTapConfirmed(MotionEvent event) {
 
-            /*
+
             if (listener != null) {
                 float x = event.getX() - offsetX;
                 float y = event.getY() - offsetY;
@@ -627,9 +627,9 @@ public class MapView extends BasicHexagonGridView {
                     listener.onCellClick(ci - 1, cj + (ci % 2) - ((cy < cellHeight / 2) ? 1 : 0));
                 }
 
-            }*/
+            }/*
             Point p = pxtoHex((int)(event.getX() - offsetX), (int) (event.getY() - offsetY));
-            listener.onCellClick(p.x, p.y);
+            listener.onCellClick(p.x, p.y);*/
             return true;
         }
 
